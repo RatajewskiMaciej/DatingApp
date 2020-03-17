2d95b7e5f43197f7a6f9c26d8cd7174aa69a8d85
RatajewskiMaciej <ratajewskimaciej@wp.pl>
2020-03-17 20:55:08 +0100

    add register function

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const config = require('config');
+const cors = require('cors');
+const register = require('./routes/register')
 const app = express();
 
 
+//Database connection
+const mongoURI = config.get('mongoURI');
+mongoose.connect(
+  mongoURI,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => {
+    console.log("DB connected")
+  }
+);
+
+//extra
+app.use(express.json())
+app.use(cors())
+
+//routing
+app.use('/', register)
+
+
+//Server running
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => { console.log(`Server is runnign on port: ${PORT}`) })
