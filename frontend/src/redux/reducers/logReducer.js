fb137923d93c799e972ef52ff439ab00845a5def
RatajewskiMaciej <ratajewskimaciej@wp.pl>
2020-03-17 23:40:10 +0100

    add redux add login

---
+++ frontend/src/redux/reducers/logReducer.js
@@ -0,0 +1,22 @@
+import { } from '../types';
+
+
+const initialState = {
+  logs: null,
+  current: null,
+  loading: false,
+  error: null
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET_LOGS:
+      return {
+        ...state,
+        logs: action.payload,
+        loading: false
+      };
+    default:
+      return state;
+  }
+}
